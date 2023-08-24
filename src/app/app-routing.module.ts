9b723ddc4c3de5509d649453277de3d2dd4cf729
SayraPa <98625428+Minori35@users.noreply.github.com>
2023-08-24 09:20:09 -0600

    rutas hijas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,32 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthModule } from './auth/auth.module';
+import { Error404pageComponent } from './shared/page/error404page/error404page.component';
 
 const routes: Routes = [
   {
-    
-  }
+    path: 'auth', 
+    loadChildren : () => import('./auth/auth.module').then(  m => m.AuthModule)
+  },
+  {
+    path: 'heroes', 
+    loadChildren : () => import('./heroes/heroes.module').then(  m => m.HeroesModule)
+  },
+  {
+    path: '404', 
+    component : Error404pageComponent
+  },
+  {
+    path: '', 
+    redirectTo : 'heroes',
+    pathMatch : 'full'
+  },
+  {
+    path: '**', 
+    redirectTo : '404',
+
+  },
+
 ];
 
 @NgModule({
