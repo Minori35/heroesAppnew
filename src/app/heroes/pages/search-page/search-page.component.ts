610ef55c474b5288ce73df7703448740e2ccf27b
SayraPa <98625428+Minori35@users.noreply.github.com>
2023-08-30 12:19:42 -0600

    Agregar Heoe HTML

--- src/app/heroes/pages/search-page/search-page.component.ts
+++ src/app/heroes/pages/search-page/search-page.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { HeroService } from '../../services/heroes.service';
 import { FormControl } from '@angular/forms';
+import { Hero } from '../../interfaces/hero.interface';
+import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
 
 @Component({
   selector: 'app-search-page',
@@ -15,6 +17,32 @@ export class SearchPageComponent {
   ){}
 
   public searchInput = new FormControl('');
-  
+  public heroes : Hero[]=[];
+
+  public selectedHero? : Hero;
+
+  searchHero(){
+    const value : string = this.searchInput.value || '';
+
+    this.heroService.getSuggestions(value)
+    .subscribe(heroes=> this.heroes= heroes);
+    console.log(this.heroes);
+    
+
+  }
+
+  onSelectedOption( event : MatAutocompleteSelectedEvent){
+    if(! event.option.value){
+      this.selectedHero = undefined
+      return
+    } 
+
+    const hero : Hero = event.option.value;
+    this.searchInput.setValue(hero.superhero);
+    this.selectedHero= hero
+    
+
+  }
+
 
 }
