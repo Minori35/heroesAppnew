c9805824f704873f5e17b8f224b57dff3e22de49
SayraPa <98625428+Minori35@users.noreply.github.com>
2023-08-31 11:35:25 -0600

    authServices

---
+++ src/app/auth/services/auth.service.ts
@@ -0,0 +1,32 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { enviroments } from 'src/enviroments/environments';
+import { User } from '../interfaces/user.interface';
+import { Observable , tap} from "rxjs";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  private baseURL = enviroments.baseUrl;
+  private user?: User;
+
+  constructor( private http: HttpClient ) { }
+
+  get currentUSer(): User | undefined{
+    if(!this.user) return undefined;
+    return structuredClone(this.user);
+  }
+
+  login(email: string, password : string):Observable<User>{
+   return this.http.get<User>(`${this.baseURL}/user/1`)
+    .pipe(
+      tap(user => this.user=user),
+        tap(user => localStorage.setItem('token', user.id.toString()))
+      
+    )
+    
+
+  }
+}
